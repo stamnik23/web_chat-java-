4fc3e823c96ed1f61991f02bd81e1916c4e21144
Alex Ts <alex_tsal@yahoo.gr>
2019-03-03 13:46:55 +0200

    changes_3_3_2019

---
+++ src/main/java/org/realtime/dao/models/PrivateChat.java
@@ -0,0 +1,45 @@
+
+package org.realtime.dao.models;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "PrivateChat")
+
+
+
+public class PrivateChat implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Type(type = "uuid-char")
+    private UUID externalId;
+
+
+    //@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "privatechat")
+    //Set<PrivateMessages> privatemessages;
+     
+	@ManyToOne(fetch=FetchType.EAGER)
+    private User user_initialiazer;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    private User user_receiver;
+	
+
+  //  @OneToMany(fetch = FetchType.EAGER,mappedBy = "rooms")
+  //  private RoomUserTracker roomUserTracker;
+
+    public Rooms() {
+        this.externalId = UUID.randomUUID();
+    }
+
+}
---
+++ src/main/java/org/realtime/dao/models/PrivateMessages.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.realtime.dao.models;
+
+
+import java.io.BufferedReader;
+import org.hibernate.annotations.Type;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.sql.Types;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "PrivateMessages")
+
+
+
+public class PrivateMessages implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Timestamp time;
+    private boolean seen;
+
+    @Type(type = "uuid-char")
+    private UUID externalId;
+
+    @Type(type = "text")
+    private String content;
+
+  //  @OneToMany(mappedBy = "messages")
+  //   private List<Rooms> rooms;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private PrivateChat privatechat;
+
+    public Messages(){
+        this.externalId = UUID.randomUUID();
+    }
+
+}
